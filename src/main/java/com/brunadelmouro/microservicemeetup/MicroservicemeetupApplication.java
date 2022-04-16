4b01a1dd44726eaa8ceb1d721de2edb9a7cc65fa
Bruna <brunadelmouro@gmail.com>
2022-04-16 16:30:15 -0300

    Initial commit

---
+++ src/main/java/com/brunadelmouro/microservicemeetup/MicroservicemeetupApplication.java
@@ -0,0 +1,13 @@
+package com.brunadelmouro.microservicemeetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicemeetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicemeetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunadelmouro/microservicemeetup/MicroservicemeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunadelmouro.microservicemeetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicemeetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

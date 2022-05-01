d5e85caed5bc3f6d889305f4ab3c14f0f2fdae21
Bruna <brunadelmouro@gmail.com>
2022-05-01 14:16:41 -0300

    Configurando swagger para Spring Security

--- src/main/java/com/brunadelmouro/microservicemeetup/config/SecurityConfig.java
+++ src/main/java/com/brunadelmouro/microservicemeetup/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -43,6 +44,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/api/registrations/**",
             "/api/meetups/**",
     };
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+                "/swagger-ui/**", "/webjars/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {

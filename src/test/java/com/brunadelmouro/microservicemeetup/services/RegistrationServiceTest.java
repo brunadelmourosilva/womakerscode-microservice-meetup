7588f864727137bb62cb054492ded887e1dd6505
Bruna Delmouro <brunadelmouro@gmail.com>
2022-05-02 00:53:57 -0300

    Testes em RegistrationService parte 3

--- src/test/java/com/brunadelmouro/microservicemeetup/services/RegistrationServiceTest.java
+++ src/test/java/com/brunadelmouro/microservicemeetup/services/RegistrationServiceTest.java
@@ -13,7 +13,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 
 @ExtendWith(SpringExtension.class)
@@ -30,23 +33,6 @@ public class RegistrationServiceTest {
     public void setUp(){
         this.registrationService = new RegistrationServiceImpl(registrationRepository);
     }
-
-    //consertar
-//    @Test
-//    @DisplayName("Should return true when create a Registration with unique e-mail")
-//    public void testUniqueEmailWhenCreatingNewRegistration(){
-//
-//        //cenário
-//        String email = "brunadelmouro@gmail.com";
-//
-//        //execução
-//        Mockito.doNothing().when(registrationRepository.existsByEmail(email));
-//
-//        registrationService.validateRegistrationExistsByEmail(email);
-//
-//        //assert
-//        verify(registrationService,times(1)).validateRegistrationExistsByEmail(email);
-//    }
 
     @Test
     @DisplayName("Should save a Registration")
@@ -66,66 +52,67 @@ public class RegistrationServiceTest {
         assertThat(registration.getNumber()).isEqualTo("001");
     }
 
-    //consertar
-//    @Test
-//    @DisplayName("Should return a Registration by id")
-//    public void findRegistrationByIdTest(){
-//        Registration registration = createValidRegistration();
-//
-//        Mockito.when(registrationRepository.findById(1)).thenReturn(Optional.of(registration));
-//
-//        assertThat(registrationService.findRegistrationById(1)).isEqualTo(registration);
-//    }
+
+    @Test
+    @DisplayName("Should return a Registration by id")
+    public void findRegistrationByIdTest(){
+        Registration registration = createValidRegistration();
+
+        Mockito.when(registrationRepository.findById(1)).thenReturn(Optional.of(registration));
+
+        assertThat(registrationService.findRegistrationById(1)).isEqualTo(registration);
+    }
 
 
-//
-//    @Test
-//    @DisplayName("Should get a Registration by number")
-//    public void getRegistrationByRegistration(){
-//
-//        String numberAttribute = "1234";
-//
-//        Mockito.when(registrationRepository.findByNumber(numberAttribute))
-//                .thenReturn(new Registration(1, numberAttribute));
-//
-//        Registration registration = registrationService.findRegistrationByRegistrationNumber(numberAttribute);
-//        Optional<Registration> foundRegistrationOp = Optional.ofNullable(registration);
-//
-//        Assertions.assertThat(foundRegistrationOp.isPresent()).isTrue();
-//        Assertions.assertThat(foundRegistrationOp.get().getId()).isEqualTo(1);
-//        Assertions.assertThat(foundRegistrationOp.get().getNumber()).isEqualTo(numberAttribute);
-//    }
-//
-//    //cenário de erro
-//    @Test
-//    @DisplayName("Should return empty when get registration by id doesn't exists")
-//    public void registrationNotFoundByIdTest(){
-//
-//        // cenário
-//        Integer id = 11;
-//        Mockito.when(registrationRepository.findById(id)).thenReturn(Optional.empty());
-//
-//        // execução
-//        Registration registration = registrationService.findRegistrationById(id);
-//
-//        Optional<Registration> foundRegistrationOp = Optional.ofNullable(registration);
-//
-//        //assert
-//        Assertions.assertThat(foundRegistrationOp.isPresent()).isFalse();
-//    }
-//
-//    @Test
-//    @DisplayName("Should delete a Registration")
-//    public void deleteRegistrationTest(){
-//
-//        //cenário
-//        Registration registration = createValidRegistration();
-//
-//        //assert
-//        assertDoesNotThrow(() -> registrationService.deleteRegistration(registration));
-//
-//        Mockito.verify(registrationRepository, Mockito.times(1)).delete(registration);
-//    }
+
+    @Test
+    @DisplayName("Should get a Registration by number")
+    public void getRegistrationByRegistration(){
+
+        String numberAttribute = "001";
+
+        Mockito.when(registrationRepository.findByNumber(numberAttribute))
+                .thenReturn(new Registration(1, numberAttribute));
+
+        Registration registration = registrationService.findRegistrationByRegistrationNumber(numberAttribute);
+        Optional<Registration> foundRegistrationOp = Optional.ofNullable(registration);
+
+        assertThat(foundRegistrationOp.isPresent()).isTrue();
+        assertThat(foundRegistrationOp.get().getId()).isEqualTo(1);
+        assertThat(foundRegistrationOp.get().getNumber()).isEqualTo(numberAttribute);
+    }
+
+
+    //cenário de erro
+    @Test
+    @DisplayName("Should return empty when get Registration by id doesn't exists")
+    public void registrationNotFoundByIdTest(){
+
+        // cenário
+        Integer id = 11;
+        Mockito.when(registrationRepository.findById(id)).thenReturn(Optional.empty());
+
+        // execução
+        Registration registration = registrationService.findRegistrationById(id);
+
+        Optional<Registration> foundRegistrationOp = Optional.ofNullable(registration);
+
+        //assert
+        assertThat(foundRegistrationOp.isPresent()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Should delete a Registration")
+    public void deleteRegistrationTest(){
+
+        //cenário
+        Registration registration = createValidRegistration();
+
+        //assert
+        assertDoesNotThrow(() -> registrationService.deleteRegistration(registration));
+
+        Mockito.verify(registrationRepository, Mockito.times(1)).delete(registration);
+    }
 
     public Registration createValidRegistration(){
         return new Registration(101, "Bruna", "bruna@inovags.com", "123", "001");
